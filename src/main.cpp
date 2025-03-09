16e1ae10ff863dd68dcc8f1ed5ec8c6248da5252
Roger Chen <rogerchen803@gmail.com>
2025-03-08 20:15:02 -0700

    CLI tests (cli-test-1741490102535)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+
+void handle_exit(std::string code)
+{
+    int error_code;
+    // std::cout << " ::: " << code << " ::: \n";
+    try
+    {
+        error_code = stoi(code);
+    }
+    catch (std::exception &e)
+    {
+        error_code = 0;
+    }
+
+    exit(error_code);
+}
 
 int main()
 {
@@ -6,11 +24,31 @@ int main()
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    std::string input;
+    std::string raw_input;
+    std::stringstream ss;
+    std::vector<std::string> input;
+
     while (1)
     {
         std::cout << "$ ";
-        std::getline(std::cin, input);
-        std::cout << input << ": command not found\n";
+        std::getline(std::cin, raw_input);
+        ss.str(raw_input);
+
+        std::string tmp;
+        while (ss >> tmp)
+            input.push_back(tmp);
+
+        if (input.size() == 0)
+            break;
+
+        if (input[0] == "exit")
+        {
+            handle_exit((input.size() == 1 ? "0" : input[1]));
+        }
+        else
+        {
+
+            std::cout << raw_input << ": command not found\n";
+        }
     }
 }
