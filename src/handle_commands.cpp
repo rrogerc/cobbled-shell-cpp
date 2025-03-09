d4e0465de721787e1737d458051a8e2ed871a2da
Roger Chen <rogerchen803@gmail.com>
2025-03-09 14:29:26 -0600

    codecrafters submit [skip ci]

---
+++ src/handle_commands.cpp
@@ -0,0 +1,53 @@
+#include "handle_commands.h"
+#include "utils.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+void handle_exit(std::string code)
+{
+    int error_code;
+    // std::cout << " ::: " << code << " ::: \n";
+    try
+    {
+        error_code = stoi(code);
+    }
+    catch (std::exception &e)
+    {
+        error_code = 0;
+    }
+
+    exit(error_code);
+}
+
+void handle_echo(std::vector<std::string> &args)
+{
+    int n = args.size();
+
+    for (int i = 1; i < n; i++)
+        std::cout << args[i] << " ";
+    std::cout << "\n";
+}
+
+void handle_type(std::string command, std::vector<std::string> &path)
+{
+    std::vector<std::string> builtins = {"echo", "type", "exit"};
+
+    for (auto i : builtins)
+    {
+        // std::cout << i << " :\n";
+        if (command == i)
+        {
+            std::cout << command << " is a shell builtin\n";
+            return;
+        }
+    }
+
+    std::string path_env_location = find_path(command, path);
+
+    if (path_env_location == "")
+        std::cout << command << ": not found\n";
+    else
+        std::cout << command << " is " << path_env_location << "\n";
+}
---
+++ src/utils.cpp
@@ -0,0 +1,32 @@
+#include "utils.h"
+
+#include <cstdlib>
+#include <filesystem>
+#include <vector>
+
+void get_path_env(std::vector<std::string> &path)
+{
+    std::string raw_path = std::getenv("PATH");
+    // std::cout << raw_path << "\n";
+
+    raw_path += ":";
+    while (raw_path.length())
+    {
+        int delimeter = raw_path.find(":");
+        path.push_back(raw_path.substr(0, delimeter));
+        raw_path.erase(0, delimeter + 1);
+    }
+}
+
+std::string find_path(std::string command, std::vector<std::string> &path)
+{
+    std::string full_path;
+    for (int i = 0; i < path.size(); i++)
+    {
+        full_path = path[i] + "/" + command;
+
+        if (std::filesystem::exists(full_path))
+            return full_path;
+    }
+    return "";
+}
