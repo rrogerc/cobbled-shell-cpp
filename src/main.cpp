8f749bc7f96352a2c41c2249452de4c8020b30b7
Roger Chen <rogerchen803@gmail.com>
2025-03-08 19:16:28 -0700

    first

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+int main()
+{
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+    std::string input;
+    std::getline(std::cin, input);
 }
