880c60a14564bb3f1acb497c4e64a39808bc2d3b
Roger Chen <rogerchen803@gmail.com>
2025-03-09 00:07:45 -0700

    CLI tests (cli-test-1741504065869)

---
+++ src/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS
+#define UTILS
+
+#include <algorithm>
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+#include <sstream>
+#include <vector>
+void get_path_env(std::vector<std::string> &path);
+
+#endif
