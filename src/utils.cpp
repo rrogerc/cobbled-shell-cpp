f865fe8cdd6668058f9e1ad6290c32a432e6db9b
Roger Chen <rogerchen803@gmail.com>
2025-03-11 10:09:20 -0600

    add support for single quotes

--- src/utils.cpp
+++ src/utils.cpp
@@ -29,3 +29,33 @@ std::string find_path(std::string command, std::vector<std::string> &path)
     }
     return "";
 }
+
+#include <iostream>
+
+void parse_input(std::string raw_input, std::vector<std::string> &input)
+{
+    input.clear();
+    std::string cur = "";
+    bool is_quote = false;
+
+    raw_input += ' ';
+
+    for (int i = 0; i < raw_input.length(); i++)
+    {
+        if (raw_input[i] == '\'')
+        {
+            is_quote = !is_quote;
+            continue;
+        }
+
+        if (is_quote || raw_input[i] != ' ')
+            cur += raw_input[i];
+        else if (cur != "")
+            input.push_back(cur), cur = "";
+    }
+
+    // for (auto i : input)
+    //     std::cout << i << "\n";
+}
+
+// echo '/tmp/quz/f   55' '/tmp/quz/f   44' '/tmp/quz/f   40' cat '/tmp/quz/f   55' '/tmp/quz/f   44' '/tmp/quz/f   40'
