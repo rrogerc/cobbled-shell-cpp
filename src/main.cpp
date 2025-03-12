1dad05b775a7bf5f7b892659a5d3b512cd00a041
Roger Chen <rogerchen803@gmail.com>
2025-03-11 22:29:42 -0600

    CLI tests (cli-test-1741753782502)

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,7 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
+int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -17,8 +16,7 @@ int main()
     std::vector<std::string> path;
     get_path_env(path);
 
-    while (1)
-    {
+    while (1) {
         std::cout << "$ ";
         std::getline(std::cin, raw_input);
         parse_input(raw_input, input);
@@ -29,30 +27,19 @@ int main()
         // for (auto i : input)
         //     std::cout << i << "\n";
 
-        if (input[0] == "exit")
-        {
+        if (input[0] == "exit") {
             handle_exit((input.size() == 1 ? "0" : input[1]));
-        }
-        else if (input[0] == "echo")
-        {
+        } else if (input[0] == "echo") {
             handle_echo(input);
-        }
-        else if (input[0] == "type")
-        {
-            if (input.size() == 1)
+        } else if (input[0] == "type") {
+            if (input.size() <= 2)
                 continue;
-            handle_type(input[1], path);
-        }
-        else if (input[0] == "pwd")
-        {
+            handle_type(input[2], path);
+        } else if (input[0] == "pwd") {
             handle_pwd();
-        }
-        else if (input[0] == "cd")
-        {
+        } else if (input[0] == "cd") {
             handle_cd(input);
-        }
-        else
-        {
+        } else {
             if (!handle_execution(input, path))
                 std::cout << raw_input << ": command not found\n";
         }
