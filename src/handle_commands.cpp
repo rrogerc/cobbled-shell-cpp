a2910ece0833c6e249acaabc4c7d2fde6759424b
Roger Chen <rogerchen803@gmail.com>
2025-03-11 22:26:45 -0600

    CLI tests (cli-test-1741753605761)

--- src/handle_commands.cpp
+++ src/handle_commands.cpp
@@ -5,43 +5,53 @@
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
-void handle_exit(std::string code)
-{
+void handle_exit(std::string code) {
     int error_code;
     // std::cout << " ::: " << code << " ::: \n";
-    try
-    {
+    try {
         error_code = stoi(code);
-    }
-    catch (std::exception &e)
-    {
+    } catch (std::exception &e) {
         error_code = 0;
     }
 
     exit(error_code);
 }
 
-void handle_echo(std::vector<std::string> &args)
-{
-    int n = args.size();
+void handle_echo(std::vector<std::string> &input) {
+    int n = input.size();
+    std::set<char> special_back = {'\\', '$', '\"'};
 
-    for (int i = 1; i < n; i++)
-        std::cout << args[i] << " ";
+    for (int i = 2; i < n; i++) {
+        if (input[i][0] == '\'' && input[i][input[i].length() - 1] == '\'')
+            std::cout << input[i].substr(1, input[i].length() - 2);
+        else if (input[i][0] == '\"' && input[i][input[i].length() - 1] == '\"') {
+            std::string cur = input[i], cur_parse = "";
+
+            for (int j = 1; j < cur.length() - 1; j++) {
+                if (cur[j] == '\\') {
+                    cur_parse += cur[++j];
+                    continue;
+                }
+                cur_parse += cur[j];
+            }
+            std::cout << cur_parse;
+        } else
+            std::cout << input[i];
+    }
+
     std::cout << "\n";
 }
 
-void handle_type(std::string command, std::vector<std::string> &path)
-{
+void handle_type(std::string command, std::vector<std::string> &path) {
     std::vector<std::string> builtins = {"echo", "type", "exit", "pwd"};
 
-    for (auto i : builtins)
-    {
+    for (auto i : builtins) {
         // std::cout << i << " :\n";
-        if (command == i)
-        {
+        if (command == i) {
             std::cout << command << " is a shell builtin\n";
             return;
         }
@@ -55,8 +65,7 @@ void handle_type(std::string command, std::vector<std::string> &path)
         std::cout << command << " is " << path_env_location << "\n";
 }
 
-bool handle_execution(std::vector<std::string> &input, std::vector<std::string> &path)
-{
+bool handle_execution(std::vector<std::string> &input, std::vector<std::string> &path) {
     std::string execution_path = find_path(input[0], path);
     if (execution_path == "")
         return false;
@@ -64,7 +73,7 @@ bool handle_execution(std::vector<std::string> &input, std::vector<std::string> 
     std::string command = input[0];
 
     for (int i = 1; i < input.size(); i++)
-        command += " \"" + input[i] + '"';
+        command += " " + input[i];
 
     // std::cout << command << "\n";
 
@@ -73,13 +82,11 @@ bool handle_execution(std::vector<std::string> &input, std::vector<std::string> 
     return true;
 }
 
-void handle_pwd()
-{
+void handle_pwd() {
     std::cout << std::filesystem::current_path().string() << "\n";
 }
 
-void handle_cd(std::vector<std::string> &input)
-{
+void handle_cd(std::vector<std::string> &input) {
     std::string to_string = (input.size() == 1 ? "~" : input[1]);
 
     if (to_string == "~")
@@ -87,12 +94,9 @@ void handle_cd(std::vector<std::string> &input)
 
     std::filesystem::path to(to_string);
 
-    try
-    {
+    try {
         std::filesystem::current_path(to);
-    }
-    catch (std::exception &e)
-    {
+    } catch (std::exception &e) {
         std::cout << "cd: " << to_string << ": No such file or directory\n";
     }
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -3,25 +3,21 @@
 #include <filesystem>
 #include <vector>
 
-void get_path_env(std::vector<std::string> &path)
-{
+void get_path_env(std::vector<std::string> &path) {
     std::string raw_path = std::getenv("PATH");
     // std::cout << raw_path << "\n";
 
     raw_path += ":";
-    while (raw_path.length())
-    {
+    while (raw_path.length()) {
         int delimeter = raw_path.find(":");
         path.push_back(raw_path.substr(0, delimeter));
         raw_path.erase(0, delimeter + 1);
     }
 }
 
-std::string find_path(std::string command, std::vector<std::string> &path)
-{
+std::string find_path(std::string command, std::vector<std::string> &path) {
     std::string full_path;
-    for (int i = 0; i < path.size(); i++)
-    {
+    for (int i = 0; i < path.size(); i++) {
         full_path = path[i] + "/" + command;
 
         if (std::filesystem::exists(full_path))
@@ -32,33 +28,42 @@ std::string find_path(std::string command, std::vector<std::string> &path)
 
 #include <iostream>
 
-void parse_input(std::string raw_input, std::vector<std::string> &input)
-{
+void parse_input(std::string raw_input, std::vector<std::string> &input) {
     input.clear();
     std::string cur = "";
-    bool is_quote = false;
 
     raw_input += ' ';
 
-    for (int i = 0; i < raw_input.length(); i++)
-    {
-        if (raw_input[i] == '\'' || raw_input[i] == '"')
-        {
+    for (int i = 0; i < raw_input.length(); i++) {
+        if (raw_input[i] == '\'' || raw_input[i] == '"') {
             int j = i + 1;
-            while (raw_input[j] != raw_input[i])
-                cur += raw_input[j++];
-            i = j;
-            continue;
-        }
-
-        if (is_quote || raw_input[i] != ' ')
-        {
-            if (raw_input[i] == '\\')
-                i++;
             cur += raw_input[i];
-        }
-        else if (cur != "")
+            while (raw_input[j] != raw_input[i]) {
+                if (raw_input[i] == '"' && raw_input[j] == '\\')
+                    cur += raw_input[j++];
+                cur += raw_input[j++];
+            }
+            cur += raw_input[i];
+            i = j;
             input.push_back(cur), cur = "";
+        } else if (raw_input[i] != ' ') {
+            int j = i;
+            while (raw_input[j] != ' ' && raw_input[j] != '\'' && raw_input[j] != '"') {
+                if (raw_input[j] == '\\')
+                    j++;
+                cur += raw_input[j++];
+            }
+            i = j - 1;
+            input.push_back(cur), cur = "";
+        } else {
+            int j = i;
+            while (raw_input[j] == ' ') {
+                j++;
+            }
+            i = j - 1;
+            if (input.size() > 0)
+                input.push_back(" ");
+        }
     }
 
     // for (auto i : input)
